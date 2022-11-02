d622ddcc150ebf0d2639f81c51b2af736398e033
andriideikalo <wlada801@gmail.com>
2022-11-02 23:53:06 +0200

    add hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const imgGalery = document.querySelector(`.gallery`);
+const markup = galleryItems.map(({ preview, original, description }) =>
+    `<a href="${original}" class = "galery__link" ><img src="${preview}" data-src="${original}"alt="${description}" class = "gallery__image" width ="340">
+     </a>`).join(``);
+// console.log(markup);
+imgGalery.insertAdjacentHTML(`beforeend`, markup);
+// let instance = ``;
+imgGalery.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    // console.log(evt.currentTarget);
+    // console.dir(evt.target);
+    // console.dir(evt.target.dataset.src);
+
+    const instance = basicLightbox.create(`<img src=${evt.target.dataset.src}>`);
+    instance.show();
+    document.addEventListener('keydown', onModalCloseToEscape);
+
+    function onModalCloseToEscape(event) {
+        if (event.code === "Escape") {
+            instance.close();
+            document.removeEventListener("keydown", onModalCloseToEscape);
+
+        }
+    }
+
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imgGalery = document.querySelector(`.gallery`);
+const markup = galleryItems.map(({ preview, original, description }) =>
+    `<a href="${original}" class = "galery__item" ><img src="${preview}" data-src="${original}"alt="${description}" class = "gallery__image" width ="340">
+     </a>`).join(``);
+// console.log(markup);
+imgGalery.insertAdjacentHTML(`beforeend`, markup);
+// let instance = ``;
+imgGalery.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    // console.log(evt.currentTarget);
+    // console.dir(evt.target);
+    // console.dir(evt.target.dataset.src);
+
+    let gallery = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+
+    document.addEventListener('keydown', onModalCloseToEscape);
+
+    function onModalCloseToEscape(event) {
+        if (event.code === "Escape") {
+            instance.close();
+            document.removeEventListener("keydown", onModalCloseToEscape);
+
+        }
+    }
+
+});
+
+// console.log(galleryItems);
