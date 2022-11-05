1e6d8b3b024f42078fb6a82098d8cc97800026e8
andriideikalo <wlada801@gmail.com>
2022-11-05 08:54:24 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 const imgGalery = document.querySelector(`.gallery`);
 const markup = galleryItems.map(({ preview, original, description }) =>
-    `<a href="${original}" class = "galery__item" ><img src="${preview}" data-src="${original}"alt="${description}" class = "gallery__image" width ="340">
+    `<a href="${original}" class = "galery__item" ><img src="${preview}" data-src="${original}"alt="${description}" class = "gallery__image" >
      </a>`).join(``);
 // console.log(markup);
 imgGalery.insertAdjacentHTML(`beforeend`, markup);
@@ -11,7 +12,4 @@ imgGalery.insertAdjacentHTML(`beforeend`, markup);
 let gallery = new SimpleLightbox('.gallery a', {
     captionsData: "alt",
     captionDelay: 250,
-});
-gallery.on('show.simplelightbox', function(evt) {
-    evt.preventDefault();
 });
